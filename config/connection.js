46a740e7dfec6b4369ee96b58850e014e4f872f9
Khadijah Grimes <khadijahgrimes7@gmail.com>
2019-12-19 20:43:01 -0600

    heroku pls

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,21 @@
 var mysql = require("mysql");
-connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "cat123",
-    database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: "cat123",
+        database: 'burgers_db'
+    });
+};
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting:" + err.stack);
         return;
     }
     console.log("connected as id" + connection.threadId);
-
-    if (process.env.JAWSDB_URL) {
-        connection = mysql.createConnection(process.env.JAWSDB_URL);
-    } else {
-        connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: "cat123",
-            database: 'burgers_db'
-        });
-    };
 
 });
 
